3eaeb367d9a53c4a2f8dad686faa4e75e39f41c4
ShikhaBakshi <31121556+ShikhaBakshi@users.noreply.github.com>
2019-02-27 11:26:38 +0530

    Update tcp-rack-test.cc

--- src/internet/test/tcp-rack-test.cc
+++ src/internet/test/tcp-rack-test.cc
@@ -1,6 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2015 Natale Patriciello <natale.patriciello@gmail.com>
+ * Copyright (c) 2018 NITK Surathkal
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,6 +15,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ * Authors: Shikha Bakshi <shikhabakshi912@gmail.com>
+ *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
  */
 #include "ns3/log.h"
 #include "ns3/tcp-westwood.h"
